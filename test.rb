a9e447dc8ed3b30eca5d29226393816714eb8e63
AkiraKodama <kodama@appirits.com>
2017-04-10 10:04:18 +0900

    test

--- test.rb
+++ test.rb
@@ -1,1 +1,56 @@
-p Hello
+=begin
+class Robot
+  attr_reader :x, :y
+  def initialize
+    @x = 0
+    @y = 0
+  end
+
+  def job
+    x0, y0 = x, y
+    yield(self)
+    p "(#{x0}, #{y0}) => (#{x}, #{y})"
+  end
+
+  def move(d1, d2)
+    @x += d1
+    @y += d2
+  end
+end
+
+robot = Robot.new
+
+robot.job { |r|
+  r.move(1, 0)
+  r.move(0, 1)
+}
+
+robot.job { |r|
+  r.move(1, 0)
+  r.move(1, 1)
+  r.move(0, -3)
+}
+=end
+
+=begin
+class Cat
+  attr_reader :value
+  def initialize
+    @value = 100
+  end
+  def where_self
+    self.value
+  end
+end
+
+cat = Cat.new.where_self
+=end
+
+=begin
+def test_method(&block)
+  proc = Proc.new {|a| a * a}
+  p proc.call(2)
+end
+
+test_method
+=end
